cf30ff75bf70aa49b57fb973d78f32968d171446
kst <kstost@gmail.com>
2020-06-28 03:29:52 +0900

    Update AudioManager.js

--- AudioManager.js
+++ AudioManager.js
@@ -2,12 +2,15 @@
    var root = this;
    var AudioManager = function (sound_resource, com_cb) {
       var pointer = this;
+      if (sound_resource === undefined) { sound_resource = {} } //SAFE
       pointer.sound_resource = sound_resource;
       pointer.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
       pointer.soundThread = {};
       pointer.uname = 'snd';
       pointer.ucnt = 0;
-      pointer.load_urls(com_cb);
+      if (Object.keys(sound_resource).length > 0) { //SAFE
+         pointer.load_urls(com_cb);
+      }
    };
    AudioManager.prototype = {
       stop: function (key_) {
@@ -36,10 +39,25 @@
       },
       add_url: function (url, key, com_cb) {
          var pointer = this;
-         pointer.sound_resource[key] = url;
-         pointer.load_urls(com_cb);
+         if (url.constructor.name === 'Array') {
+            //SAFE
+            com_cb = key;
+            url.forEach(url_ => {
+               pointer.sound_resource[url_] = url_;
+            });
+            pointer.load_urls(com_cb);
+         } else {
+            //SAFE
+            if (key === undefined) { key = url; }
+            pointer.sound_resource[key] = url;
+            pointer.load_urls(com_cb);
+         }
       },
       load_urls: function (com_cb) {
+         //SAFE
+         if (!com_cb) {
+            com_cb = () => { }
+         }
          var pointer = this;
          var url_list = [];
          var cb_count = 0;
@@ -59,6 +77,7 @@
                pointer.audio_to_b64(url_list[i], function (base64, err, url, key) {
                   cb_count++;
                   if (err) {
+                     delete pointer.sound_resource[key]; //SAFE
                      err_count.push({
                         err: err,
                         url: url,
@@ -93,7 +112,10 @@
          }
       },
       grant_permission: function () {
-         this.play();
+         //SAFE
+         if (this.audioCtx.state === 'suspended') {
+            this.play();
+         }
       },
       play: function (sound_name, ecb) {
          var pointer = this;
@@ -107,12 +129,52 @@
             }
          });
       },
+      //SAFE
+      isLoaded: function (key) {
+         let pointer = this;
+         if (pointer.sound_resource[key]) {
+            return pointer.sound_resource[key].constructor.name === 'Object';
+         }
+         return false;
+      },
+      isLoading: function (key) {
+         let pointer = this;
+         if (pointer.sound_resource[key]) {
+            return pointer.sound_resource[key].constructor.name !== 'Object';
+         }
+         return false;
+      },
+      //SAFE
+      run: function (url, ecb) {
+         let pointer = this;
+         if (pointer.audioCtx.state === 'suspended') {
+            pointer.grant_permission();
+         }
+         if (pointer.isLoaded(url)) {
+            this.play(url, ecb);
+         } else if (pointer.isLoading(url)) {
+         } else {
+            pointer.add_url(url, url, (err) => {
+               if (!(err && err.filter(row => row.url === url).length)) {
+                  this.play(url, ecb);
+               } else {
+                  if (ecb && ecb.error) {
+                     ecb.error();
+                  }
+               }
+            });
+         }
+      },
       audio_to_b64: function (key, cb) {
          var pointer = this;
          var url = pointer.sound_resource[key];
          var request = new XMLHttpRequest();
          request.open('GET', url, true);
          request.responseType = 'arraybuffer';
+         //SAFE
+         request.onerror = function () {
+            cb(null, 'network fail', url, key);
+         };
          request.onload = function () {
             if (request.status == 200) {
                pointer.audioCtx.decodeAudioData(request.response, function (buffer) {
@@ -133,7 +195,7 @@
       extract_sound: function (name, cb) {
          var pointer = this;
          var cb_exe = false;
-         if (pointer.sound_resource[name] === undefined) {
+         if (!pointer.isLoaded(name) && !pointer.isLoading(name) && name === 'grant_key') { //SAFE
             pointer.sound_resource[name] = {};
             var channels = 2;
             var frameCount = pointer.audioCtx.sampleRate * 0.00003;
@@ -146,7 +208,7 @@
             }
             pointer.sound_resource[name].buffer = myArrayBuffer;
          }
-         if (pointer.sound_resource[name] !== undefined) {
+         if (pointer.isLoaded(name)) { //SAFE
             if (!pointer.sound_resource[name].play) {
                pointer.sound_resource[name].play = function (ecb) {
                   pointer.ucnt++;
@@ -162,17 +224,57 @@
                      source: source
                   };
                   source.buffer = this.buffer;
-                  source.connect(this.audioCtx.destination);
+                  //SAFE
+                  var gainNode = this.audioCtx.createGain();
+                  source.connect(gainNode);
+                  gainNode.connect(this.audioCtx.destination);
+                  gainNode.gain.value = name === 'grant_key' ? 0 : 1;
+                  let startTime = this.audioCtx.currentTime;
+                  let raf = (function (callback) {
+                     return window.requestAnimationFrame ||
+                        window.webkitRequestAnimationFrame ||
+                        window.mozRequestAnimationFrame ||
+                        window.oRequestAnimationFrame ||
+                        window.msRequestAnimationFrame ||
+                        function (callback) { window.setTimeout(callback, 1000 / 60); };
+                  })();
+                  let recur_ = () => {
+                     if (pointer.soundThread[pid]) {
+                        let spent = (this.audioCtx.currentTime - startTime);
+                        let keep = true;
+                        if (ecb && ecb.playtime) {
+                           if (ecb.playtime / 1000 <= spent) {
+                              keep = false;
+                           }
+                        }
+                        if (spent > 0 && ecb && ecb.start) {
+                           ecb.start(sdt);
+                           delete ecb.start;
+                        }
+                        if (ecb && ecb.progress && !ecb.start) {
+                           let ratio = spent / source.buffer.duration;
+                           if (ratio > 1) { ratio = 1; }
+                           ecb.progress(sdt, ratio);
+                        }
+                        if (keep) {
+                           raf(recur_);
+                        } else {
+                           pointer.stop_by_pid(pid);
+                        }
+                     }
+                  }
                   source.onended = function () {
+                     //SAFE
+                     if (ecb && ecb.progress) {
+                        ecb.progress(sdt, 1);
+                     }
                      delete pointer.soundThread[pid];
                      if (ecb && ecb.end) {
                         ecb.end(sdt);
                      }
                   };
                   source.start();
-                  if (ecb && ecb.start) {
-                     ecb.start(sdt);
-                  }
+                  recur_();
                };
                pointer.sound_resource[name].audioCtx = pointer.audioCtx;
                if (!pointer.sound_resource[name].buffer) {
@@ -181,6 +283,8 @@
                      pointer.sound_resource[name].buffer = buffer;
                      delete pointer.sound_resource[name].base64;
                      cb(pointer.sound_resource[name]);
+                  }, function (err) { //SAFE
+                     delete pointer.sound_resource[name];
                   });
                }
             }
